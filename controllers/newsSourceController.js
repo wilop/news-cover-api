05fc6a1a1005f8de7858f85b9a997e2d3194bbe0
gespinozac-utn <gespinozac@utn.ac.cr>
2023-03-13 15:06:07 -0600

    add process in news source to get the feed and add news

--- controllers/newsSourceController.js
+++ controllers/newsSourceController.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { model: NewsSourceModel } = require('../models/newsSourceModel');
 const { model: UserModel } = require('../models/userModel');
 const { model: CategoryModel } = require('../models/categoryModel');
-const rssParser = require('rss-parser');
+const Parser = require('rss-parser');
 
 router.get('/', async (req, res) => {
     NewsSourceModel.find({ "user.email": res.locals.session.email })
@@ -131,7 +131,53 @@ router.delete('/id=:id', (req, res) => {
 });
 
 router.post('/:id/process', async (req, res) => {
-    console.log(res.locals.session)
+    const parser = new Parser();
+    const { model: NewsModel } = require('../models/newsModel');
+    const newsSources = await NewsSourceModel.find({ "user.email": res.locals.session.email });
+    const newsAdded = [];
+
+    try {
+        for (const element of newsSources) {
+            const feed = await parser.parseURL(element.url)
+            for (const item of feed.items) {
+                console.log(item)
+                const newNews = NewsModel();
+                newNews.title = item.title;
+                newNews.short_description = item.contentSnippet || item.content || "No info";
+                newNews.permalink = item.link;
+                newNews.date = item.pubDate;
+                newNews.news_source = element;
+                newNews.user = element.user;
+                newNews.category = element.category;
+                await newNews.save()
+                    .then(newNewsAdded => {
+                        newsAdded.push(newNewsAdded);
+                    })
+                    .catch(err => {
+                        res
+                            .status(422)
+                            .json({
+                                Message: err
+                            })
+                    })
+            }
+        }
+        if (newsAdded.length > 0) {
+            res
+                .status(201)
+                .json({
+                    model: "news",
+                    data: newsAdded
+                })
+        }
+    } catch (err) {
+        console.log("err",err)
+        res
+            .status(422)
+            .json({
+                Message: err
+            })
+    }
 });
 
 module.exports = router;
